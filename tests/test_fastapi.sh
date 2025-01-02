65f7cca50aee29db042fa8d971fbe33b1c9468d8
Aaron Belenky <aaron.b@sealsecurity.io>
2025-01-02 14:08:23 +0200

    change time machine to correct date

--- tests/test_fastapi.sh
+++ tests/test_fastapi.sh
@@ -8,7 +8,7 @@ git fetch --tags
 latest_tag_commit=$(git rev-list --tags --max-count=1)
 latest_tag=$(git describe --tags "${latest_tag_commit}")
 git checkout "0.79.0"
-pip install --force-reinstall --index-url "https://:2022-09-02T00:17:14.391Z@time-machines-pypi.sealsecurity.io/" setuptools wheel
-pip install --index-url "https://:2022-09-02T00:17:14.391Z@time-machines-pypi.sealsecurity.io/" .[all,dev,doc,test]
+pip install --force-reinstall --index-url "https://:2022-09-05T00:17:14.391Z@time-machines-pypi.sealsecurity.io/" setuptools wheel
+pip install --index-url "https://:2022-09-05T00:17:14.391Z@time-machines-pypi.sealsecurity.io/" .[all,dev,doc,test]
 
 ./scripts/test.sh
