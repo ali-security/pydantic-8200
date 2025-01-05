93fbb48b8031046cc0bfbe103df9cd9a3fafa5d2
Aaron Belenky <aaron.b@sealsecurity.io>
2025-01-05 14:03:54 +0200

    1.10.0+sp1

--- tests/test_fastapi.sh
+++ tests/test_fastapi.sh
@@ -7,8 +7,8 @@ cd fastapi
 git fetch --tags
 latest_tag_commit=$(git rev-list --tags --max-count=1)
 latest_tag=$(git describe --tags "${latest_tag_commit}")
-git checkout "${latest_tag}"
-
-pip install .[all,dev,doc,test]
+git checkout "0.79.0"
+pip install --force-reinstall --index-url "https://:2022-09-05T00:17:14.391Z@time-machines-pypi.sealsecurity.io/" setuptools wheel
+pip install --index-url "https://:2022-09-05T00:17:14.391Z@time-machines-pypi.sealsecurity.io/" .[all,dev,doc,test]
 
 ./scripts/test.sh
